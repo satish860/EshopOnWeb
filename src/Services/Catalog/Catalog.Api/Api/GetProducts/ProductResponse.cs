9ff7bd009dbdf913c6087c95ee51c87746fb5cc0
satish <satish860@gmail.com>
2022-02-25 12:13:29 +0530

    Created a Catalog Endpoint
    Created a Initial Catalog Endpoint for the Catalog

---
+++ src/Services/Catalog/Catalog.Api/Api/GetProducts/ProductResponse.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.Api.Entities;
+
+namespace Catalog.Api.Api.GetProducts
+{
+    public class ProductResponse
+    {
+        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
+    }
+}
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,10 @@
+using FastEndpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddFastEndpoints();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseFastEndpoints();
 
 app.Run();
