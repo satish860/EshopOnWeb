3cf6128f5a7d10f535c26620a54216eb7198ee15
satish <satish860@gmail.com>
2022-02-26 19:10:54 +0530

    Added Swagger Support. This closes #1

--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,6 +1,6 @@
 using Basket.Api.Data;
 using FastEndpoints;
-using Microsoft.Extensions.DependencyInjection;
+using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
@@ -9,8 +9,11 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 builder.Services.AddTransient<IBasketRepository, BasketRespository>();
+builder.Services.AddSwaggerDoc();
 var app = builder.Build();
 
 
 app.UseFastEndpoints();
+app.UseOpenApi();
+app.UseSwaggerUi3();
 app.Run();
