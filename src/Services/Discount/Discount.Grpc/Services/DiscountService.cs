08907ca50b6f14b0f73b8f4d2dfa9097ae289cea
satish <satish860@gmail.com>
2022-02-28 12:21:35 +0530

    Corrected the Build Error

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService:DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override Task<GetDiscountResponse> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+    }
+}
