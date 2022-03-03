293b8635ea6ecd264b2adc3f4bbe121ff4c2f298
satish <satish860@gmail.com>
2022-03-03 18:20:20 +0530

    Added Catalog to Reverse Proxy

--- src/ApiGateways/ReverseProxy/Program.cs
+++ src/ApiGateways/ReverseProxy/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("yarp"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapReverseProxy();
 
 app.Run();
