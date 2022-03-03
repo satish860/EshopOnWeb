36dbeecb92fd095dbdc1c240a98c4522a2cc6df5
satish <satish860@gmail.com>
2022-03-03 13:28:17 +0530

    Added Event Consumer for the Project. closes #24

---
+++ src/Services/Ordering/Ordering.Api/EventBus.Consumers/BasketCheckoutEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using EventBus.Messages.Events;
+using MassTransit;
+using Ordering.Api.Domain;
+using Ordering.Api.Repository;
+
+namespace Ordering.Api.EventBus.Consumers
+{
+    public class BasketCheckoutEventHandler : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public BasketCheckoutEventHandler(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            Order order = new Order
+            {
+                AddressLine = context.Message.AddressLine,
+                CardName = context.Message.CardName,
+                CardNumber = context.Message.CardNumber,
+                Country = context.Message.Country,
+                CVV = context.Message.CVV,
+                EmailAddress = context.Message.EmailAddress,
+                Expiration = context.Message.Expiration,
+                FirstName = context.Message.FirstName,
+                LastName = context.Message.LastName,
+                PaymentMethod = context.Message.PaymentMethod,
+                State = context.Message.State,
+                TotalPrice = context.Message.TotalPrice,
+                UserName = context.Message.UserName,
+                ZipCode = context.Message.ZipCode,
+            };
+            await this.orderRepository.CreateOrder(order);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,11 +1,27 @@
+using EventBus.Messages.Common;
 using FastEndpoints;
+using MassTransit;
 using Ordering.Api.Data;
+using Ordering.Api.EventBus.Consumers;
 using Ordering.Api.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
 builder.Services.AddTransient<IOrderContext, OrderContext>();
 builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<BasketCheckoutEventHandler>();
+    x.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetValue<string>("Rabbitmq:HostUrl"));
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => {
+            c.ConfigureConsumer<BasketCheckoutEventHandler>(ctx);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
 var app = builder.Build();
 
 app.UseFastEndpoints();
