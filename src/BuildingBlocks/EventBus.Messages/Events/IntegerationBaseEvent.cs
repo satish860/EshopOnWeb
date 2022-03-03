d3b23d056de337680016fcfd79846a1f9e9daa5f
satish <satish860@gmail.com>
2022-03-03 11:32:06 +0530

    Added Building blocks and projects. This closes #21

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegerationBaseEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegerationBaseEvent
+    {
+        public IntegerationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        public IntegerationBaseEvent(Guid id,DateTime createdDateTime)
+        {
+            Id=id;
+            CreatedDate = createdDateTime;
+        }
+
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
