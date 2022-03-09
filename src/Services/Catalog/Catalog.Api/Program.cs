492722e461aa98813c5339fc9d82f0a216e8e148
satish <satish860@gmail.com>
2022-03-09 19:08:59 +0530

    Serilog added to the Configuration.

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -2,15 +2,19 @@ using Catalog.Api.Data;
 using Catalog.Api.Repositories;
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddSwaggerDoc();
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .ReadFrom.Configuration(ctx.Configuration));
 var app = builder.Build();
 
-
+app.UseSerilogRequestLogging();
 app.UseFastEndpoints();
 app.UseOpenApi(); //add this
 app.UseSwaggerUi3(s => s.ConfigureDefaults());
